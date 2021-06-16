7a1a47ae9d87a02d3168f3c6a234a2ab5763809f
Aman Gupta <aman@keio.jp>
2021-06-16 11:57:55 +0900

    add grid layout for whole page

---
+++ layout/main.js
@@ -0,0 +1,15 @@
+toggleDoor = (event) => {
+  //   console.log(event.target.nodeName);
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   if (event.target !== event.currentTarget) {
+  //     return;
+  //   }
+  //   console.log(event);
+  var snd = new Audio("sound/car-door-open-1.wav");
+  snd.play();
+  var door = event.target.getAttribute("data-door");
+  console.log("door_id:", door);
+  var element = document.getElementById(door);
+  element.classList.toggle(door + "-toggle");
+};
